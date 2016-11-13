2dd5018b0bb9f71467de0f32319da61a4140eb41
csaron92 <csaron92@gmail.com>
2016-11-13 13:21:23 +0200

    MagicMap added

--- NokiaTowerCPP/NokiaTowerCPP/player.cpp
+++ NokiaTowerCPP/NokiaTowerCPP/player.cpp
@@ -11,7 +11,7 @@ int maxPop(int pop[MAP_SIZE][MAP_SIZE], int &x, int &y)
 {
 	int i = 0;
 	int j = 0;
-	double temp = 0;
+	int temp = 0;
 	for (i = 0; i < MAP_SIZE; i++)
 	{
 		for (j = 0; j < MAP_SIZE; j++)
@@ -32,13 +32,10 @@ double getDistance(int x, int y, int j, int k) {
 	return std::sqrt(std::pow((x - j), 2) + std::pow(y - k, 2));
 }
 
-double maxPopTower(int towers[][2], bool towerMap[MAP_SIZE][MAP_SIZE], int &x, int &y) {
+int maxPopTower(int towers[][2], bool towerMap[MAP_SIZE][MAP_SIZE], int &x, int &y) {
 
-	int temp_x, temp_y = 0, ID = 0;
+	int ID = 0;
 	double min_distance = 100000000;
-
-
-	
 
 	for(int i = 0; i < TOWER_MAX; i++)
 	{
@@ -46,8 +43,7 @@ double maxPopTower(int towers[][2], bool towerMap[MAP_SIZE][MAP_SIZE], int &x, i
 		{
 			min_distance = getDistance(x, y, towers[i][0], towers[i][1]);
 			ID = i;
-		}
-		
+		}		
 	}
 
 	cout << " min dist: " << min_distance << endl;
@@ -85,9 +81,17 @@ void TPlayer::makeMove() {
         }
     }
     else {
-        if (inputData.header.time == 1) rentTower(124, 10, 35, 100);
+
+		cout << "money:" << inputData.header.money << endl;
+		if (inputData.header.time == 1) {
+			rentTower(27,7.1, 3, 1);
+			rentTower(28, 8.01, 4, 1);
+			rentTower(29, 9.001, 5, 1);
+			rentTower(30, 10.0001, 6, 1);
+			
+		}
         if (inputData.header.time == 5) changeDistanceAndOffer(124, 20, 110);
-        if (inputData.header.time > 10 && inputData.header.time < 120) outputData.invest = inputData.header.time;
+        if (inputData.header.time > 10 && inputData.header.time < 220) outputData.invest = inputData.header.money * 0.08;
 		if (inputData.header.time == 10) 
 		{
 			cout << "time: " << inputData.header.time << " max pop:" << maxPop(map->pop,maxPopLocationX, maxPopLocationY ) << endl;
@@ -106,6 +110,87 @@ void TPlayer::makeMove() {
 		
         //if (inputData.header.time == 140) leaveTower(124);
     }
+}
+
+
+class MagicMap {
+	int population[MAP_SIZE][MAP_SIZE];
+	int towers[TOWER_MAX][2];
+public:
+	void set_values(int [MAP_SIZE][MAP_SIZE], int [TOWER_MAX][2]);
+	void give_me_my_magic_map(int *[MAP_SIZE][MAP_SIZE]);
+	void give_me_my_tower_population(int *[MAP_SIZE/30][MAP_SIZE/30], int *[][2], int *[(MAP_SIZE / 30)*(MAP_SIZE / 30)][2]);
+
+};
+
+void MagicMap::set_values(int pop[MAP_SIZE][MAP_SIZE], int tow[TOWER_MAX][2]) {
+	for (int i = 0; i < MAP_SIZE; i++)
+	{
+		for (int j = 0; j < MAP_SIZE; j++)
+		{
+			population[i][j] = pop[i][j];
+		}
+	}
+
+	for (int i = 0; i < TOWER_MAX; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			towers[i][j] = tow[i][j];
+		}
+	}
+}
+
+void MagicMap::give_me_my_magic_map(int *population[MAP_SIZE][MAP_SIZE]) {
+
+	int magicMap[MAP_SIZE / 30][MAP_SIZE / 30];
+
+
+	for (int magic_x = 0; magic_x < MAP_SIZE/30; magic_x++)
+	{
+		for (int magic_y = 0; magic_y < MAP_SIZE / 30; magic_y++)
+		{	
+			for (int pop_x = magic_x *30; pop_x < magic_x *30 + 30; pop_x++)
+			{
+				for (int pop_y = magic_y *30; pop_y < magic_y *30 + 30; pop_y++)
+				{
+					magicMap[magic_x][magic_y] += (int) population[pop_x][pop_y];
+				}
+			}
+		}
+		
+	}
+}
+
+void MagicMap::give_me_my_tower_population(int *magicMap[MAP_SIZE/30][MAP_SIZE/30], int *towers[][2], int *population_with_tower_id[(MAP_SIZE/30)*(MAP_SIZE/30)][2]) {
+	int bigmap_district_center_x = 0;
+	int bigmap_district_center_y = 0;
+	int tower_ID = 0;
+
+
+	/* get tower ID for all districts */
+	for (int magic_x = 0; magic_x < MAP_SIZE/30 ; magic_x++)
+	{
+		for (int magic_y = 0; magic_y < MAP_SIZE/30; magic_y++)
+		{
+			bigmap_district_center_x = 15 + 30 * magic_x;
+			bigmap_district_center_y = 15 + 30 * magic_y;
+			double min_distance = 10000;
+
+			/* get nearest tower to district center */
+			for (int i = 0; i < TOWER_MAX; i++)
+			{
+				if (min_distance > getDistance(bigmap_district_center_x, bigmap_district_center_y, *towers[i][0], *towers[i][1]))
+				{
+					min_distance = getDistance(bigmap_district_center_x, bigmap_district_center_y, *towers[i][0], *towers[i][1]);
+					tower_ID = i;
+				}
+			}
+
+			*population_with_tower_id[magic_x * (MAP_SIZE / 30) + magic_y][0] = *magicMap[magic_x][magic_y]; // population in district
+			*population_with_tower_id[magic_x * (MAP_SIZE / 30) + magic_y][1] = tower_ID;					 // nearest tower id
+		}
+	}
 }
 
 
